4341e8fb2fb74f9ae111c932bd1ee0406881dc4f
Alex Ferguson <adferg@pdx.edu>
2017-12-31 13:50:49 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 /*
 GAME RULES:
-
 - The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
+- In each turn, a player rolls two dice as many times as he/she wishes. Each result gets added to the player's ROUND score
+- BUT, if the player rolls a 1 for either dice, all his/her ROUND score gets lost. After that, it's the next player's turn
+- If the player consecutively rolls two 6's, not on a single roll but across two rolls, then that player loses their entire
+  GLOBAL score and it's the next players turn.
+- The player can choose to 'Hold', which means that their ROUND score gets added to their GLOBAL score. 
+  After that, it's the next player's turn
+- The first player to reach the appropriate amount of points on their GLOBAL score wins the game
 
 */
 
